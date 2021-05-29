8174dd85b8bfb9c87e16178b7fe6304f5445d869
Chandramani Vyas <chandramani09022001@gmail.com>
2021-05-29 22:41:34 +0530

    size

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import './App.css';
-import { CardList } from './components/card-list/card-list';
-import { SearchBox } from './components/Search-field/searchBox';
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/Search-field/searchBox";
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
 
-    this.state ={
-      monsters:[],
-      searchField:"",
+    this.state = {
+      monsters: [],
+      searchField: "",
     };
-  
   }
-  
-  componentDidMount()
-  {
-    fetch('https://jsonplaceholder.typicode.com/users').then(response=>
-    response.json())
-    .then(user=>this.setState({monsters:user})
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({ monsters: user }));
+  }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
-  }
-  handleChange=(e)=>{
-    this.setState({searchField:e.target.value});
-  }
-  render(){
-    const {monsters,searchField }=this.state;
-    const filteredMonster=monsters.filter(monster=>
-      monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
-      <div className='App'>
-        <SearchBox placeholder='Search kitty-monsters'
-        handleChange={this.handleChange}/>
-        <CardList monsters={filteredMonster} > 
-        
-        </CardList>
+      <div className="App">
+        <SearchBox
+          placeholder="Search kitty-monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster}></CardList>
       </div>
     );
   }
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './card-style.css';
-export const Card= props =>
-{
-   return(<div className="card-container">
-       <img src={`https://robohash.org/${props.monster.id}?set=set4`}></img>
-       <h2 >{props.monster.name}</h2>
-       <p>{props.monster.email}</p>
-    </div>);
-}
+import React from "react";
+import "./card-style.css";
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img src={`https://robohash.org/${props.monster.id}?set=set4`}></img>
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
